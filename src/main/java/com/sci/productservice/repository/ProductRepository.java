e01594841c2743f347d9f32dae067f16224f78e2
anandhumohan <anandhumohan5@gmail.com>
2024-01-22 10:42:15 +0000

    log added

--- src/main/java/com/sci/productservice/repository/ProductRepository.java
+++ src/main/java/com/sci/productservice/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.sci.productservice.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.sci.productservice.model.Product;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String>{
 
 }
