8f6fa0dc4bc2e568d6c0945188e499d1f517f35d
anandhumohan <anandhumohan5@gmail.com>
2024-01-22 10:41:30 +0000

    log added

--- src/main/java/com/sci/productservice/model/Product.java
+++ src/main/java/com/sci/productservice/model/Product.java
@@ -1,14 +1,13 @@
 package com.sci.productservice.model;
 
-import java.math.BigDecimal;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
 
 @Document(value = "product")
 @Data
